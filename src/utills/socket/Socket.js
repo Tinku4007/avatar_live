4121f9c9cd5b6c6c1245c5a93faa5e3fa442c030
Tinku4007 <tinkusaini1252@gmail.com>
2024-09-06 12:24:04 +0530

    update

--- src/utills/socket/Socket.js
+++ src/utills/socket/Socket.js
@@ -4,7 +4,7 @@ import { getLocalStorage } from "@/utills/LocalStorageUtills";
 
 
 // const socket = io(`${import.meta.env.VITE_APP_VITEAPPLOCAL}/`, {
-const socket = io(`http://localhost:3000/`, {
+const socket = io(`https://backend-avatar-local.onrender.com/`, {
   reconnectionAttempts: 5, // Number of reconnection attempts before giving up
   transports: ['websocket'], // Use WebSocket transport
   secure: true, // Secure connection should be true for wss://, but for http use false
